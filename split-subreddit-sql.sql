b13d7efb33dff1d0674483a04bcaee048671d666
timendum <timedum@gmail.com>
2025-05-22 16:59:21 +0200

    sql: archive by year

--- split-subreddit-sql.sql
+++ split-subreddit-sql.sql
@@ -9,3 +9,19 @@ CREATE TABLE old.comments AS SELECT * FROM comments WHERE created_utc < STRFTIME
 CREATE TABLE old.comments_awards AS SELECT * FROM comments_awards AS c WHERE EXISTS (SELECT id FROM old.comments WHERE comments.id = c.comment_id);
 
 CREATE TABLE old.traffics AS SELECT * FROM traffics WHERE day < STRFTIME('%s', DATE('now','start of month')) AND day > STRFTIME('%s', DATE('now','start of month', '-1 month'));
+
+-- DELETE
+
+DELETE FROM submissions WHERE EXISTS (select * from old.submissions as otable where submissions.id = otable.id);
+DELETE FROM comments WHERE EXISTS (select * from old.comments as otable where comments.id = otable.id);
+DELETE FROM submissions_awards WHERE EXISTS (select * from old.submissions_awards as otable where submissions_awards.id = otable.id);
+DELETE FROM comments_awards WHERE EXISTS (select * from old.comments_awards as otable where comments_awards.id = otable.id);
+
+-- ON OLD DB
+
+
+CREATE UNIQUE INDEX submissions_id ON submissions(id);
+CREATE UNIQUE INDEX comments_id ON comments(id);
+CREATE UNIQUE INDEX submissions_awards_id ON submissions_awards(id);
+CREATE UNIQUE INDEX comments_awards_id ON comments_awards(id);
+CREATE UNIQUE INDEX traffics_day ON traffics(day);
